70e6ce15cfbc914460285bcfd53b6186530a4722
Rushikesh Dane <78649021+rushikeshdane@users.noreply.github.com>
2024-09-20 20:45:59 +0530

    Update 03_About.py

--- pages/03_About.py
+++ pages/03_About.py
@@ -3,10 +3,18 @@ import streamlit as st
 
 st.title('Hi, I am Rushikesh Dane')
 
-st.markdown('<p style="border-radius: 20px;color:blue;font-size:20px;">I am data science enthusiastic , '
+st.markdown("""<hr style="height:7px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
+
+st.markdown('<p style="border-radius: 20px;color:black;font-size:20px;">I am data science enthusiastic , '
              'who believes that words and data are the two most powerful tools to change the world.'
              'my skills include data analysis, visualization, and machine learning '
             ' I will link my work to  GitHub repositories, so feel free to download my code'
             ' and play around with it.</p>', unsafe_allow_html=True)
 
+st.markdown("""<hr style="height:7px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
+
 st.subheader('Github   [link](https://github.com/rushikeshdane/Nashik-Apartment-Price-Prediction)')
+st.subheader('Linkedin  [link](www.linkedin.com/in/rushikesh-dane-2918a6242)')
+
+
+st.markdown("""<hr style="height:7px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
